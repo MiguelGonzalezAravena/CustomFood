9b70587e443816fac97bffc58a871f41db7f9b59
Miguel González Aravena <mgonzalez@produccion.datactil.com>
2016-06-01 19:37:29 -0400

    Actualizar dependencias front-end

--- files.js
+++ files.js
@@ -1,12 +1,31 @@
 module.exports = {
   js: [
-    'bower_components/angular/angular.js'
+    'bower_components/jquery/dist/jquery.js'
+  , 'bower_components/datatables/media/js/jquery.dataTables.js'
+  , 'bower_components/angular/angular.js'
+  , 'bower_components/bootstrap/dist/js/bootstrap.js'
   , 'bower_components/angular-bootstrap/ui-bootstrap.js'
+  , 'bower_components/angular-route/angular-route.js'
+  , 'bower_components/trix/dist/trix.js'
+  , 'bower_components/angular-trix/dist/angular-trix.js'
+  , 'bower_components/angular-datatables/dist/angular-datatables.js'
+  , 'bower_components/bootbox/bootbox.js'
+  , 'bower_components/ngBootbox/dist/ngBootbox.js'
   , 'private/js/main.js'
   ],
   css: [
-    'bower_components/angular-bootstrap/ui-bootstrap-csp.css'
+    'bower_components/bootstrap/dist/css/bootstrap.css'
+  , 'bower_components/angular-bootstrap/ui-bootstrap-csp.css'
+  , 'bower_components/trix/dist/trix.css'
+  , 'bower_components/angular-datatables/dist/css/angular-datatables.css'
+  , 'bower_components/datatables/media/css/dataTables.bootstrap.css'
+  , 'bower_components/datatables/media/css/jquery.dataTables.css'
   , 'private/css/main.css'
   ],
-  assets: {}
+  assets: {
+    'bower_components/bootstrap/fonts/*': 'fonts'
+  , 'private/angular-bootstrap/uib/template/typeahead/*': 'uib/template/typeahead'
+  , 'private/angular-bootstrap/uib/template/rating/*': 'uib/template/rating'
+  , 'views/*.html': 'pages'
+  }
 };
